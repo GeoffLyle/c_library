47e5abb7620a5e0b97a7dc937d9cd522fbf09771
Alfred Lyle <geofflyle@gmail.com>
2018-05-12 14:36:35 -0700

    Add ft_memccpy.c

---
+++ src/ft_memccpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/12 14:17:26 by alyle             #+#    #+#             */
+/*   Updated: 2018/05/12 14:36:01 by alyle            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n)
+{
+	char		*d;
+	const char	*s;
+
+	if (n)
+	{
+		d = dst;
+		s = src;
+		while (n-- && *s != c)
+			*d++ = *s++;
+	}
+	return (dst);
+}
