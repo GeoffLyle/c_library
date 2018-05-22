6b4ce9c259afd58ff1627a04c45321906ea02bea
Geoff Lyle <geofflyle@gmail.com>
2018-05-21 22:09:52 -0700

    gst

--- src/ft_putnbr_fd.c
+++ src/ft_putnbr_fd.c
@@ -9,10 +9,10 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n < 0)
 	{
-		write(fd, '-', 1);
+		write(fd, "-", 1);
 		if (n == -2147483648)
 		{
-			write(fd, '2', 1);
+			write(fd, "2", 1);
 			n = -147483648;
 		}
 		n *= -1;
