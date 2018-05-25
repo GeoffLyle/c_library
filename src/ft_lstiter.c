c0e93a099959942612016a0d041c85bbe310295b
Alfred Lyle <geofflyle@gmail.com>
2018-05-25 16:40:16 -0700

    Add more list functions

--- src/ft_lstiter.c
+++ src/ft_lstiter.c
@@ -1,6 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/25 16:37:16 by alyle             #+#    #+#             */
+/*   Updated: 2018/05/25 16:38:56 by alyle            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(t_list elem))
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
-
+	if (lst)
+	{
+		while (lst->next)
+		{
+			f(lst);
+			lst = lst->next;
+		}
+		f(lst);
+	}
 }
