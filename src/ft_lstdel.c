8a0e39aab95c7e1218dbd3e7ef2222e2237c3706
Alfred Lyle <geofflyle@gmail.com>
2018-05-25 16:34:46 -0700

    Add list delete functions

--- src/ft_lstdel.c
+++ src/ft_lstdel.c
@@ -1,6 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alyle <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/25 16:25:15 by alyle             #+#    #+#             */
+/*   Updated: 2018/05/25 16:32:53 by alyle            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
+	t_list *list;
+	t_list *tmp;
 
+	if (*alst)
+	{
+		list = *alst;
+		while (list)
+		{
+			del(list->content, list->content_size);
+			tmp = list->next;
+			free(list);
+			list = NULL;
+			list = tmp;
+		}
+	}
+	*alst = NULL;
 }
