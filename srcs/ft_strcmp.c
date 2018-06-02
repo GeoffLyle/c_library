a3f74bf809a53129e46d162ce7aed2f5e20a463c
Geoff Lyle <geofflyle@gmail.com>
2018-06-02 11:13:10 -0700

    Fix strcmp casting to unsigned char

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -12,15 +12,10 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	const char	*str1;
-	const char	*str2;
-
-	str1 = s1;
-	str2 = s2;
-	while (*str1 && *str2 && *str1 == *str2)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		str1++;
-		str2++;
+		s1++;
+		s2++;
 	}
-	return (*str1 - *str2);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -17,9 +17,9 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (*s1 && *s2 && n--)
 	{
 		if (*s1 != *s2)
-			return ((unsigned char)(*s1 - *s2));
+			return ((unsigned char)*s1 - (unsigned char)*s2);
 		s1++;
 		s2++;
 	}
-	return ((unsigned char)(*s1 - *s2));
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
