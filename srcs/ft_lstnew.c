4fb54cf00c83f17a3a335ec60c8b52e3c67d5561
Geoff Lyle <geofflyle@gmail.com>
2018-06-03 23:36:49 -0700

    Fix ft_lstnew.c

--- srcs/ft_lstnew.c
+++ srcs/ft_lstnew.c
@@ -16,19 +16,23 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*tmp;
 
-	tmp = malloc(sizeof(t_list));
+	tmp = (t_list *)malloc(sizeof(t_list));
 	if (tmp)
 	{
 		if (content)
 		{
-			tmp->content = (void *)content;
+			tmp->content = malloc(sizeof(content));
+			if (tmp->content == NULL)
+				return (NULL);
 			tmp->content_size = content_size;
+			ft_memcpy(tmp->content, content, content_size);
 		}
 		else
 		{
 			tmp->content = NULL;
 			tmp->content_size = 0;
 		}
+		tmp->next = NULL;
 		return (tmp);
 	}
 	return (NULL);
