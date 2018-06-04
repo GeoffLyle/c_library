2345b7b277d53ecbf8d221ccb91b9756fbe88666
Geoff Lyle <geofflyle@gmail.com>
2018-06-03 21:31:14 -0700

    Fix string comparison functions

--- srcs/ft_strnequ.c
+++ srcs/ft_strnequ.c
@@ -14,14 +14,15 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	while (*s1 && *s2 && n--)
+	if (s1 && s2)
 	{
-		if (*s1 != *s2)
-			return (0);
-		s1++;
-		s2++;
+		while (*s1 && *s2 && n--)
+		{
+			if (*s1++ != *s2++)
+				return (0);
+		}
+		if (*s1 == *s2)
+			return (1);
 	}
-	if (*s1 == *s2)
-		return (1);
 	return (0);
 }
