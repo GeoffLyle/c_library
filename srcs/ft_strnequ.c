cadb02108435523f42a50cf54943c32b9cebb450
Geoff Lyle <geofflyle@gmail.com>
2018-06-04 19:55:14 -0700

    Fix strn compare functions

--- srcs/ft_strnequ.c
+++ srcs/ft_strnequ.c
@@ -14,14 +14,18 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
+	if (n == 0)
+		return (1);
 	if (s1 && s2)
 	{
-		while (*s1 && *s2 && n--)
+		while (*s1 && *s1 == *s2 && n--)
 		{
-			if (*s1++ != *s2++)
-				return (0);
+			s1++;
+			s2++;
 		}
-		if (*s1 == *s2)
+		if (n == 0)
+			return (1);
+		else if (*s1 == *s2)
 			return (1);
 	}
 	return (0);
